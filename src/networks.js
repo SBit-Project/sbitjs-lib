6968d7ff90ef1d26f8d0df6544e263eebe18aac1
Scot Lynne <103759613+ScotLynne@users.noreply.github.com>
2022-04-17 10:31:31 +0430

    Edit bech32 and pubKeyHash

--- src/networks.js
+++ src/networks.js
@@ -1,23 +1,23 @@
 module.exports = {
     sbit: {
         messagePrefix: '\x15SBit Signed Message:\n',
-        bech32: 'bc',
+        bech32: 'sc',
         bip32: {
             public: 0x0878c22a,
             private: 0x0878bda8
         },
-        pubKeyHash: 0x1a,
+        pubKeyHash: 0x3F,
         scriptHash: 0x32,
         wif: 0x80
     },
     sbit_testnet: {
         messagePrefix: '\x15SBit Signed Message:\n',
-        bech32: 'tb',
+        bech32: 'ts',
         bip32: {
             public: 0x084226ab,
             private: 0x08423661
         },
-        pubKeyHash: 0x55,
+        pubKeyHash: 0x7D,
         scriptHash: 0x6e,
         wif: 0xef
     }
